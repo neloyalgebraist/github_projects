524378dce784c5e6a00c142d9b6fc4cd1de816fd
neloyalgebraist <neloyalgebraist@gmail.com>
2026-04-18 20:57:31 +0530

    map-reduce

---
+++ langgraph_projects/map-reduce.py
@@ -0,0 +1,80 @@
+from re import sub
+from langchain_groq import ChatGroq
+
+# Prompts we will use
+subjects_prompt = """Generate a list of 3 sub-topics that are all related to this overall topic: {topic}."""
+joke_prompt = """Generate a joke about {subject}"""
+best_joke_prompt = """Below are a bunch of jokes about {topic}. Select the best one! Return the ID of the best one, starting 0 as the ID for the first joke. Jokes: \n\n {jokes}"""
+
+# LLM
+model = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
+
+import operator
+from typing import Annotated
+from typing_extensions import TypedDict
+from pydantic import BaseModel
+
+
+class Subjects(BaseModel):
+    subjects: list[str]
+
+
+class BestJoke(BaseModel):
+    id: int
+
+
+class OverallState(TypedDict):
+    topic: str
+    subjects: list
+    jokes: Annotated[list, operator.add]
+    best_selected_joke: str
+
+
+def generate_topics(state: OverallState):
+    prompt = subjects_prompt.format(topic=state["topic"])
+    response = model.with_structured_output(Subjects).invoke(prompt)
+    return {"subjects": response.subjects}
+
+
+from langgraph.types import Send
+
+
+def continue_to_jokes(state: OverallState):
+    return [Send("generate_joke", {"subject": s}) for s in state["subjects"]]
+
+
+class JokeState(TypedDict):
+    subject: str
+
+
+class Joke(BaseModel):
+    joke: str
+
+
+def generate_joke(state: JokeState):
+    prompt = joke_prompt.format(subject=state["subject"])
+    response = model.with_structured_output(Joke).invoke(prompt)
+    return {"jokes": [response.joke]}
+
+
+def best_joke(state: OverallState):
+    jokes = "\n\n".join(state["jokes"])
+    prompt = best_joke_prompt.format(topic=state["topic"], jokes=jokes)
+    response = model.with_structured_output(BestJoke).invoke(prompt)
+    return {"best_selected_joke": state["jokes"][response.id]}
+
+
+from langgraph.graph import END, StateGraph, START
+
+graph = StateGraph(OverallState)
+graph.add_node("generate_topics", generate_topics)
+graph.add_node("generate_joke", generate_joke)
+graph.add_node("best_joke", best_joke)
+graph.add_edge(START, "generate_topics")
+graph.add_conditional_edges("generate_topics", continue_to_jokes, ["generate_joke"])
+graph.add_edge("generate_joke", "best_joke")
+graph.add_edge("best_joke", END)
+
+app = graph.compile()
+for s in app.stream({"topic": "animals"}):
+    print(s)
---
+++ langgraph_projects/subgraph.py
@@ -0,0 +1,160 @@
+from inspect import cleandoc
+from operator import add
+import annotated_types
+from langgraph.graph.state import StateNode
+from langgraph.pregel.main import Output
+from typing_extensions import TypedDict
+from typing import List, Optional, Annotated
+
+
+# The structure of the logs
+class Log(TypedDict):
+    id: str
+    question: str
+    docs: Optional[list]
+    answer: str
+    grade: Optional[int]
+    grader: Optional[str]
+    feedback: Optional[str]
+
+
+from IPython.display import Image, display
+from langgraph.graph import StateGraph, START, END
+
+
+# Failure Analysis Sub-graph
+class FailureAnalysisState(TypedDict):
+    cleaned_logs: List[Log]
+    failures: List[Log]
+    fa_summary: str
+    processed_logs: List[str]
+
+
+class FailureAnalysisOutputState(TypedDict):
+    fa_summary: str
+    processed_logs: List[str]
+
+
+def get_failures(state):
+    """Get logs that contain a failure"""
+    cleaned_logs = state["cleaned_logs"]
+    failures = [log for log in cleaned_logs if "grade" in log]
+    return {"failures": failures}
+
+
+def generate_summary(state):
+    """Generate summary of failures"""
+    failures = state["failures"]
+    # Add fxn: fa_summary = summarize(failures)
+    fa_summary = "Poor quality retrieval of Chroma documentation."
+    return {
+        "fa_summary": fa_summary,
+        "processed_logs": [
+            f"failure-analysis-on-log-{failure['id']}" for failure in failures
+        ],
+    }
+
+
+fa_builer = StateGraph(
+    state_schema=FailureAnalysisState, output_schema=FailureAnalysisOutputState
+)
+fa_builer.add_node("get_failures", get_failures)
+fa_builer.add_node("generate_summary", generate_summary)
+fa_builer.add_edge(START, "get_failures")
+fa_builer.add_edge("get_failures", "generate_summary")
+fa_builer.add_edge("generate_summary", END)
+
+graph = fa_builer.compile()
+
+
+# Summarization Sub-graph
+class QuestionSummarizationState(TypedDict):
+    cleaned_logs: List[Log]
+    qa_summary: str
+    report: str
+    processed_logs: List[str]
+
+
+class QuestionSummarizationOutputState(TypedDict):
+    report: str
+    processed_logs: List[str]
+
+
+def generate_summary(state):
+    cleaned_logs = state["cleaned_logs"]
+    # Add fxn: summary = summarize(generate_summary)
+    report = "foo bar baz"
+    return {"report": report}
+
+
+qs_builder = StateGraph(
+    QuestionSummarizationState, output_schema=QuestionSummarizationOutputState
+)
+qs_builder.add_node("generate_summary", generate_summary)
+qs_builder.add_node("send_to_slack", send_to_slack)
+qs_builder.add_edge(START, "generate_summary")
+qs_builder.add_edge("generate_summary", "send_to_slack")
+qs_builder.add_edge("send_to_slack", END)
+
+graph = qs_builder.compile()
+
+
+# Entry Graph
+class EntryGraphState(TypedDict):
+    raw_logs: List[Log]
+    cleaned_logs: Annotated[List[Log], add]
+    fa_summary: str
+    report: str
+    processed_logs: Annotated[List[int], add]
+
+
+# Entry Graph
+class EntryGraphState(TypedDict):
+    raw_logs: List[Log]
+    cleaned_logs: List[Log]
+    fa_summary: str  # This will only be generated in the FA sub-graph
+    report: str  # This will only be generated in the QS sub-graph
+    processed_logs: Annotated[
+        List[int], add
+    ]  # This will be generated in BOTH sub-graphs
+
+
+def clean_logs(state):
+    # Get logs
+    raw_logs = state["raw_logs"]
+    # Data cleaning raw_logs -> docs
+    cleaned_logs = raw_logs
+    return {"cleaned_logs": cleaned_logs}
+
+
+entry_builder = StateGraph(EntryGraphState)
+entry_builder.add_node("clean_logs", clean_logs)
+entry_builder.add_node("question_summarization", qs_builder.compile())
+entry_builder.add_node("failure_analysis", fa_builder.compile())
+
+entry_builder.add_edge(START, "clean_logs")
+entry_builder.add_edge("clean_logs", "failure_analysis")
+entry_builder.add_edge("clean_logs", "question_summarization")
+entry_builder.add_edge("failure_analysis", END)
+entry_builder.add_edge("question_summarization", END)
+
+graph = entry_builder.compile()
+
+# Dummy logs
+question_answer = Log(
+    id="1",
+    question="How can I import ChatOllama?",
+    answer="To import ChatOllama, use: 'from langchain_community.chat_models import ChatOllama.'",
+)
+
+question_answer_feedback = Log(
+    id="2",
+    question="How can I use Chroma vector store?",
+    answer="To use Chroma, define: rag_chain = create_retrieval_chain(retriever, question_answer_chain).",
+    grade=0,
+    grader="Document Relevance Recall",
+    feedback="The retrieved documents discuss vector stores in general, but not Chroma specifically",
+)
+
+raw_logs = [question_answer, question_answer_feedback]
+graph.invoke({"raw_logs": raw_logs})
